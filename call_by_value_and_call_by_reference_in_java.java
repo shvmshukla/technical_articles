e00fee59664c310dfc67aafd6c97cb4940718c55
SHIVAM SHUKLA <shvmshukla37@gmail.com>
2018-06-09 02:30:34 +0530

    Update call_by_value_and_call_by_reference_in_java.java

--- call_by_value_and_call_by_reference_in_java.java
+++ call_by_value_and_call_by_reference_in_java.java
@@ -120,3 +120,78 @@ BUILD SUCCESSFUL (total time: 0 seconds)
 
 */
 ----------------------------------------------------------------------------------------------------------------------------
+
+
+
+/*
+Solution is to use Wrapper Class If we create a wrapper class that contains references of Car, we can swap cars by 
+swapping references of wrapper class.
+*/
+
+// A Java program to demonstrate that we can use wrapper
+// classes to swap to objects
+ 
+// A car with model and no.
+class Car
+{
+    int model, no;
+ 
+    // Constructor
+    Car(int model, int no)
+    {
+        this.model = model;
+        this.no = no;
+    }
+ 
+    // Utility method to print object details
+    void print()
+    {
+        System.out.println("no = " + no + 
+                           ", model = " + model);
+    }
+} 
+
+// A Wrapper over class that is used for swapping
+class CarWrapper
+{
+   Car c;
+ 
+   // Constructor
+   CarWrapper(Car c)   {this.c = c;}
+}
+ 
+// A Class that use Car and swaps objects of Car
+// using CarWrapper
+class Main
+{
+    // This method swaps car objects in wrappers
+    // cw1 and cw2
+    public static void swap(CarWrapper cw1, 
+                            CarWrapper cw2)
+    {
+        Car temp = cw1.c;
+        cw1.c = cw2.c;
+        cw2.c = temp;
+    }
+ 
+    // Driver method
+    public static void main(String[] args)
+    {
+        Car c1 = new Car(101, 1);
+        Car c2 = new Car(202, 2);
+        CarWrapper cw1 = new CarWrapper(c1);
+        CarWrapper cw2 = new CarWrapper(c2);
+        swap(cw1, cw2);
+        cw1.c.print();
+        cw2.c.print();
+    }
+}
+/*
+Run on IDE
+Output:
+
+no = 2, model = 202
+no = 1, model = 101
+*/
+//https://www.geeksforgeeks.org/swap-exchange-objects-java/
+----------------------------------------------------------------------------------------------------------------------------
